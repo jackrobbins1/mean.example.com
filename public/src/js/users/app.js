f9db3f30e19d32c674eb05a8b689d43927305442
Jackson Robbins <jackrobbins1@gmail.com>
2018-05-14 09:34:57 -0500

    ajax functionality

--- public/src/js/users/app.js
+++ public/src/js/users/app.js
@@ -1,6 +1,6 @@
 function viewIndex(){
     //this url down here is the "endpoint"
-    var url = 'http://localhost:3000/api/users';
+    var url = 'http://loc.mean.example.com/api/users';
 
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url);
@@ -41,7 +41,7 @@ function viewIndex(){
 }
 
 function viewUser(id){
-    var url = 'http://localhost:3000/api/users/' + id;
+    var url = 'http://loc.mean.example.com/api/users/' + id;
 
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url);
@@ -54,6 +54,9 @@ function viewUser(id){
 
         app.innerHTML = `
         <h2>${user.last_name}, ${user.first_name}</h2>
+        <span>
+            <button id="deleteUser">Delete User</button>
+        </span>
         <table class="table">
             <tbody>
                 <tr><th>ID</th><td>${user._id}</td></tr>
@@ -94,7 +97,7 @@ function viewUser(id){
         editUser.addEventListener('submit', function(e){
             e.preventDefault();
             var formData = new FormData(editUser);
-            var url = 'http://localhost:3000/api/users';
+            var url = 'http://loc.mean.example.com/api/users';
             var xhr = new XMLHttpRequest();
             xhr.open('PUT', url);
             xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
@@ -103,10 +106,106 @@ function viewUser(id){
             formData.forEach(function(value, key){
                 object[key] = value;
             });
+
             xhr.send(JSON.stringify(object));
+            xhr.onload = function(){
+                let data = JSON.parse(xhr.response);
+                if(data.success===true){
+                    viewIndex();
+                }
+            }
         });
+
+        // My delete method for the button I added.
+        var deleteUser = document.getElementById('deleteUser');
+        //variable e means 'event'
+        deleteUser.addEventListener('click', function(e){
+            e.preventDefault();
+            var url = 'http://loc.mean.example.com/api/users/' + user._id;
+            var xhr = new XMLHttpRequest();
+            xhr.open('DELETE', url);
+
+
+            xhr.send();
+            xhr.onload = function(){
+                let data = JSON.parse(xhr.response);
+                if(data.success===true){
+                    viewIndex();
+                }
+            }
+        });
+
     }
+
 
 }
 
+function createUser(){
+    var app = document.getElementById('app');
+
+    app.innerHTML = `<h2>Create a New User</h2>
+    <form id="createUser" action="/api/users" method="post">
+        <div>
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username">
+        </div>
+        <div>
+            <label for="email">Email</label>
+            <input type="text" name="email" id="email">
+        </div>
+        <div>
+            <label for="first_name">First Name</label>
+            <input type="text" name="first_name" id="first_name">
+        </div>
+        <div>
+            <label for="last_name">Last Name</label>
+            <input type="text" name="last_name" id="last_name" >
+        </div>
+
+        <input type="submit" value="Submit">
+    </form>`;
+
+    var createUser = document.getElementById('createUser');
+    //e is the variable that means 'event'
+    createUser.addEventListener('submit', function(e){
+        e.preventDefault();
+
+        var formData = new FormData(createUser);
+        var url = 'http://loc.mean.example.com/api/users';
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        
+        xhr.setRequestHeader(
+            'Content-Type',
+            'application/json; charset=UTF-8'
+        );
+
+        var object = {};
+        formData.forEach(function(value, key){
+            object[key]=value;
+        });
+
+        xhr.send(JSON.stringify(object));
+        xhr.onload = function(){
+            let data = JSON.parse(xhr.response);
+            if(data.success===true){
+                viewIndex();
+            }
+        }
+
+    });
+}
 viewIndex();
+
+var hash = window.location.hash.substr(1);
+if(hash){
+    let chunks = hash.split('-');
+
+    if(chunks[0]=='edit'){
+        viewUser(chunks[1])
+    }
+
+    if(chunks[0]=="createUser"){
+        createUser();
+    }
+}
