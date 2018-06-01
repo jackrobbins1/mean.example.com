3a60a116f9c52a3ef43b70ebe01fc1edabc61879
Jackson Robbins <jackrobbins1@gmail.com>
2018-06-01 15:48:07 -0500

    qwick fix

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(helmet());
 if(process.env.NODE_ENV==='production'){
   var config = require('../config.prod');
 }else{
-var config = require('./config.dev');
+//var config = require('./config.dev');
 }
 
 var passport = require('passport');
